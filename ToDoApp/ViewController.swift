c17dfc3f44d83deea10b7d95522b8fb9671a1ea1
Gülce önder <gulceonder@gmail.com>
2023-10-29 17:10:13 +0300

    fix

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
 
 extension ViewController:   UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return strings.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
