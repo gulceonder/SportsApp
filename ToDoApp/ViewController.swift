d26dd5cc6aa38afa5a2370b1ac2bc792b71594de
Gülce önder <gulceonder@gmail.com>
2023-10-29 17:09:35 +0300

    first table view cells visible

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var strings: [String] = ["Apple", "Monsters"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
 
 }
 
+extension ViewController:   UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = strings[indexPath.row]
+        return cell
+    }
+}
